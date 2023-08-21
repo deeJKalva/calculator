d176bb7344d115c2bad2c913e324c014fe4077a4
deeJKalva <kkalvafitri@gmail.com>
2023-08-21 17:51:17 +0800

    commit pertama

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /*membuat objek kalkulator*/
 
-const calculator = {
-  displayNumber: '0',
-  operator: null,
-  firstNumber: null,
-  waitingForSecondNumber: false
-};
+// const calculator = {
+//   displayNumber: '0',
+//   operator: null,
+//   firstNumber: null,
+//   waitingForSecondNumber: false
+// };
 
+function calcNumbers(result) {
+  form.displayResult.value = form.displayResult.value+result;
+}
+
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
